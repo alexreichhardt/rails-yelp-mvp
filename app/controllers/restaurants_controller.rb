a5682976e4bfe050ab01683ae8384004651c62b9
Alexander Reichhardt <alexander.reichhardt@whu.edu>
2018-06-27 23:26:38 +0200

    done

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,52 @@
+class RestaurantsController < ApplicationController
+
+  before_action :find_restaurant, only: [ :show, :edit, :update, :destroy ]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @reviews = Review.where(restaurant_id: params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+     if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @restaurant.update(restaurant_params)
+      @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @restaurant.destroy
+    redirect_to restaurants_path
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+end
